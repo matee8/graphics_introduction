d24e840bfb03391a0c06f39df08f793a46ddc8f8
matee8 <molnarmatenorbert@gmail.com>
2024-09-28 18:12:02 +0200

    SDL_PollEvent() instead of SDL_WaitEvent()

---
+++ include/graphics/line.h
@@ -0,0 +1,8 @@
+#ifndef GRAPHICS_LINE_H
+#define GRAPHICS_LINE_H
+
+#include <stdint.h>
+
+void mid_point_v1(uint32_t pixels[], );
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -6,7 +7,7 @@
 
 #define WIDTH 640
 #define HEIGHT 480
-#define CAL_POS(x, y) ((y) * WIDTH + (x))
+#define POS_AT(x, y) ((y) * WIDTH + (x))
 
 int32_t main(void)
 {
@@ -21,33 +22,38 @@ int32_t main(void)
 	SDL_Texture *texture =
 		SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
 				  SDL_TEXTUREACCESS_STATIC, WIDTH, HEIGHT);
+	SDL_SetRenderDrawColor(renderer, 8, 8, 8, 8);
 	SDL_Event event;
 
 	uint32_t pixels[WIDTH * HEIGHT];
-    memset(pixels, 255, WIDTH * HEIGHT * sizeof(pixels[0]));
+	memset(pixels, 255, WIDTH * HEIGHT * sizeof(pixels[0]));
 
+	uint32_t x, y;
 	while (!quit) {
 		SDL_UpdateTexture(texture, NULL, pixels,
 				  WIDTH * sizeof(pixels[0]));
 
-		SDL_WaitEvent(&event);
-
-		switch (event.type) {
-		case SDL_QUIT:
-			quit = 1;
-			break;
-		case SDL_MOUSEBUTTONUP:
-			if (event.button.button == SDL_BUTTON_LEFT)
-				lmb_down = 0;
-			break;
-		case SDL_MOUSEBUTTONDOWN:
-			if (event.button.button == SDL_BUTTON_LEFT)
-				lmb_down = 1;
-			break;
-		case SDL_MOUSEMOTION:
-			if (lmb_down)
-				pixels[CAL_POS(event.motion.x, event.motion.y)] = 0;
-			break;
+		while (SDL_PollEvent(&event)) {
+			switch (event.type) {
+			case SDL_QUIT:
+				quit = 1;
+				break;
+			case SDL_MOUSEBUTTONUP:
+				if (event.button.button == SDL_BUTTON_LEFT)
+					lmb_down = 0;
+				break;
+			case SDL_MOUSEBUTTONDOWN:
+				if (event.button.button == SDL_BUTTON_LEFT)
+					lmb_down = 1;
+				break;
+			case SDL_MOUSEMOTION:
+				x = SDL_clamp(event.motion.x, 0, WIDTH);
+				y = SDL_clamp(event.motion.y, 0, HEIGHT);
+				if (lmb_down) {
+					pixels[POS_AT(x, y)] = 0;
+				}
+				break;
+			}
 		}
 
 		SDL_RenderClear(renderer);
@@ -55,8 +61,8 @@ int32_t main(void)
 		SDL_RenderPresent(renderer);
 	}
 
-    SDL_DestroyTexture(texture);
-    SDL_DestroyRenderer(renderer);
+	SDL_DestroyTexture(texture);
+	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 
