4f7a3536d34b72ff334e5b34d934e0bdae4e75b2
matee8 <molnarmatenorbert@gmail.com>
2024-10-06 22:31:20 +0200

    Polygon can now be created from different types of lines

--- src/polygon.rs
+++ src/polygon.rs
@@ -22,19 +22,6 @@ where
 #[error("At least two points are required to create a polygon.")]
 pub struct NotEnoughPointsError;
 
-#[non_exhaustive]
-#[derive(Debug, Error, Clone)]
-pub enum PolygonFromLinesError {
-    #[error("At least 1 line is required to create a polygon.")]
-    NotEnoughLines,
-    #[error("The lines are required to touch to create a polygon.")]
-    LinesDontTouch,
-    #[error(
-        "One color polygons can only be created from the same color lines."
-    )]
-    DifferentColor,
-}
-
 impl<R> Polygon<OneColorLine, R>
 where
     R: Renderer,
@@ -63,11 +50,24 @@ where
             _renderer: PhantomData,
         })
     }
+}
 
+#[non_exhaustive]
+#[derive(Debug, Error, Clone)]
+pub enum PolygonFromLinesError {
+    #[error("At least 1 line is required to create a polygon.")]
+    NotEnoughLines,
+    #[error("The lines are required to touch to create a polygon.")]
+    LinesDontTouch,
+}
+
+impl<T, R> Polygon<T, R>
+where
+    T: LineSegment + Renderable<R> + Clone,
+    R: Renderer,
+{
     #[inline]
-    pub fn new_from_lines(
-        lines: &[OneColorLine],
-    ) -> Result<Self, PolygonFromLinesError> {
+    pub fn new_from_lines(lines: &[T]) -> Result<Self, PolygonFromLinesError> {
         if lines.is_empty() {
             return Err(PolygonFromLinesError::NotEnoughLines);
         }
@@ -81,15 +81,6 @@ where
 
         if lines[lines.len() - 1].last_point() != lines[0].first_point() {
             return Err(PolygonFromLinesError::LinesDontTouch);
-        }
-
-        if lines.first().is_some_and(|first| {
-            lines
-                .iter()
-                .skip(1)
-                .all(|elem| elem.color() == first.color())
-        }) {
-            return Err(PolygonFromLinesError::DifferentColor);
         }
 
         Ok(Self {
